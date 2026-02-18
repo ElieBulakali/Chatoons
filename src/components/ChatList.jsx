e54b901950b490c10b8abaab5b8cf04fed0f47fb
Elie BULAKALI <137182964+ElieBulakali@users.noreply.github.com>
2026-02-18 15:23:20 +0000

    changes chats

--- src/components/ChatList.jsx
+++ src/components/ChatList.jsx
@@ -3,14 +3,14 @@ export default function ChatList() {
   return (
     <div className="chat-list">
       <h2>Messages</h2>
-      <div className="chat-item active">
+      <a href="#" className="chat-item active">
         <strong>Shane Martinez</strong>
         <p>On my way home...</p>
-      </div>
-      <div className="chat-item">
+      </a>
+      <a href="#" className="chat-item">
         <strong>Katie Keller</strong>
         <p>I'm watching Friends...</p>
-      </div>
+      </a>
     </div>
   );
 }
